1a9f85bc749bdae2dea55110c497236329392f2e
Aidan <aidanc@internode.on.net>
2024-03-31 14:30:46 +1030

    rename was looking at wrong directory T - T

--- renameLowercase.sh
+++ renameLowercase.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-find /home/aidan/Documents/tst/ -depth | xargs -n 1 rename -v 's/(.*)\/([^\/]*)/$1\/\L$2/'
+
+find /arma3/steamapps/workshop/content/107410/ -depth | xargs -n 1 rename -v 's/(.*)\/([^\/]*)/$1\/\L$2/'
 
